60e6766a0b326ce6273517e0bf779f34477f5e22
Giri-Rokz <spartanprince27@gmail.com>
2020-04-03 21:35:38 +0530

    edited

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
 let canvas = document.getElementById('covid19').getContext('2d');
 canvas.font = "3em Interstate sans-serif Verdana";
 canvas.color = "white";
-canvas.fillStyle= "yellow";
-canvas.fillRect(10,15,180,130);
-canvas.strokeText("Covid19",10,50);
+canvas.fillStyle= "whitesmoke";
+canvas.fillRect(25,15,200,180);
+canvas.strokeText("Covid-19",10,30);
 
 var map;
+var markers = [];
 function initMap() {
 	var center = {lat: 11.1271, lng: 78.6569};
 	map = new google.maps.Map(document.getElementById('map'), {
 	  center: center,
-	  zoom: 8,
+	  zoom: 12,
 	  mapTypeId: google.maps.MapTypeId.ROADMAP
 	});	
 }
 
 function setup() {
 	document.getElementById('patients').addEventListener('change',this.changeMap,true);
+	Array.from(document.getElementsByTagName('li')).forEach(function(x) {
+		x.addEventListener('click',this.switchMenu,true);
+	});
+}
+
+function switchMenu(e) {
+	document.querySelector('.active').classList.remove("active");
+	e.currentTarget.classList.add("active");	
 }
 
 function changeMap(e) {
-	var latitude,longitude;
-	latitude = Number(e.currentTarget.value.split(",")[0]);
-	longitude = Number(e.currentTarget.value.split(",")[1]);
-	/*map = new google.maps.Map(document.getElementById('map'), {
-	  center: {lat: latitude, lng: longitude},
-	  zoom: 12,
-	  mapTypeId: google.maps.MapTypeId.ROADMAP
-	});*/
-	var marker = new google.maps.Marker({
-      position: {lat: latitude, lng: longitude},
-      map: map,
-	  zoom: 12
-    });
+	var value;	
+	value = e.currentTarget.value.split(",");	
+	//remove prev markers	
+	if(markers.length) {
+		for(let i=0;i<markers.length;i++) {
+			markers[i].setMap(null);	
+		}
+	}	
+	for(let i=0; i<value.length;i=i+2) {
+		var marker = new google.maps.Marker({
+		  position: {lat: Number(value[i]), lng: Number(value[i+1])},
+		  map: map,
+		});
+		markers.push(marker);
+		if ((!map.getBounds().contains(marker.getPosition()))) {
+			map.setCenter(marker.getPosition());  
+			//OR map.panTo(marker.getPosition());  
+		}
+	}
 }
 this.setup();
